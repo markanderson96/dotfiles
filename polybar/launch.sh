218a605181fb74141c28a3ec7c370cb393f109dd
Mark Anderson <andersm3@tcd.ie>
2020-06-30 20:06:08 +0100

    bspwm config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Terminate already running bars
+# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-polybar top &
-polybar bottom &
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar mybar &
+
+echo "Polybar launched..."
